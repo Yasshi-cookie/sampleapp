d0a9e298e9058a21a2c82991c0f0121fa4ea720e
Yasushi Teruya <y.teruyacookie@gmail.com>
2020-04-23 15:43:56 +0900

    Add full_name

--- example_user.rb
+++ example_user.rb
@@ -2,11 +2,16 @@ class User
     attr_accessor :name, :email
 
     def initialize(attributes = {})
-        @name  = attributes[:name]
+        @first_name  = attributes[:first_name]
+        @last_name = attributes[:last_name]
         @email = attributes[:email]
-      end
+    end
+
+    def full_name
+        @full_name = "#{@first_name} #{@last_name}"
+    end
     
-      def formatted_email
-        "#{@name} <#{@email}>"
-      end
+    def formatted_email
+        "#{@full_name} <#{@email}>"
+end
 end
