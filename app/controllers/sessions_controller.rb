2212b3094b5eea206aa7da81b1ff6d0ff9a2af6a
Yasshi-cookie <y.teruyacookie@gmail.com>
2020-05-18 11:45:12 +0900

    Improve create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,14 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+      # 「userが有効かつ、そのuserのpasswordが正しければ」の意味
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+    else
+      # エラーメッセージを作成する
+      render 'new'
+    end
   end
 
   def destroy; end
