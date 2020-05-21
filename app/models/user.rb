54df523f14dc3d438c785f635fe781d2aebcda8a
Yasshi-cookie <y.teruyacookie@gmail.com>
2020-05-21 13:46:46 +0900

    Add remember method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@
 
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+  attr_accessor :remember_token
 
   before_save { email.downcase! }
 
@@ -26,5 +27,11 @@ class User < ApplicationRecord
   # ランダムなトークンを返す
   def self.new_token
     SecureRandom.urlsafe_base64
+  end
+
+  # 永続セッションのためにユーザーをデータベースに記憶する
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 end
