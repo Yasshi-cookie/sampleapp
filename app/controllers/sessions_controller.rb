b5d8ec28ecbd4cc2fda35c36d27a0dae94f857ff
Yasshi-cookie <y.teruyacookie@gmail.com>
2020-05-21 15:16:18 +0900

    Add keep login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,16 +4,25 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    # 「userが有効かつ、そのuserのpasswordが正しければ」の意味
     if user&.authenticate(params[:session][:password])
-      log_in user
+      login_and_remember
+
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
-      # flash.nowのメッセージは、その後リクエストが発生したときに消滅する
       render 'new'
     end
+  end
+
+  private
+
+  def user
+    @user ||= User.find_by(email: params[:session][:email].downcase)
+  end
+
+  def login_and_remember
+    log_in user
+    remember user
   end
 
   def destroy
