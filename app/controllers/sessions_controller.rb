77e09e4258fbae8c08ead01474a7f1ceac173d00
Yasshi-cookie <y.teruyacookie@gmail.com>
2020-05-21 17:49:25 +0900

    finish remember me checkbox

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     if user&.authenticate(params[:session][:password])
       login_and_remember
-
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
