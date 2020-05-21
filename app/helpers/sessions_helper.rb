5a80acf4cfea351b5717a9b7f7d8e30f393d6068
Yasshi-cookie <y.teruyacookie@gmail.com>
2020-05-21 19:00:52 +0900

    remove blackets and Improve gemfile

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -15,9 +15,9 @@ module SessionsHelper
 
   # 記憶トークンcookieに対応するユーザーを返す
   def current_user
-    if (user_id = session[:user_id])
+    if user_id = session[:user_id]
       @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.signed[:user_id])
+    elsif user_id = cookies.signed[:user_id]
       user = User.find_by(id: user_id)
       if user&.authenticated?(cookies[:remember_token])
         log_in user
