41ed8bf79907cf9528d7a21c83c4917776651d10
Yasshi-cookie <y.teruyacookie@gmail.com>
2020-05-22 18:19:36 +0900

    Add index page

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -3,7 +3,8 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  test 'layout links' do
+  # ログインしていない場合のテスト
+  test 'layout links when not logged in' do
     get root_path
     assert_template 'static_pages/home'
     assert_select 'a[href=?]', root_path, count: 2
@@ -15,5 +16,23 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select 'title', full_title('Contact')
     get signup_path
     assert_select 'title', full_title('Sign up')
+  end
+
+  def setup
+    @user = users(:michael)
+  end
+  # ログインしている場合のテスト
+  test 'layout links when logged in' do # ホームページ用の統合テスト
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select 'a[href=?]', root_path, count: 2
+    assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', users_path
+    assert_select 'a[href=?]', user_path(@user)
+    assert_select 'a[href=?]', edit_user_path(@user)
+    assert_select 'a[href=?]', logout_path
+    assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', contact_path
   end
 end
