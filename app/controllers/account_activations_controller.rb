e56d06c1e9786bd6125ad424efb6171f755552b6
Yasshi-cookie <y.teruyacookie@gmail.com>
2020-05-29 18:56:35 +0900

    Add test of activation and refactoring

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -2,9 +2,8 @@
 
 class AccountActivationsController < ApplicationController
   def edit
-    user = User.find_by(email: params[:email])
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
-      update_attribute_and_log_in_user
+      activate_and_log_in_user
       flash[:success] = 'Account activated!'
       redirect_to user
     else
@@ -15,9 +14,12 @@ class AccountActivationsController < ApplicationController
 
   private
 
-  def update_attribute_and_log_in_user
-    user.update_attribute(:activated,    true)
-    user.update_attribute(:activated_at, Time.zone.now)
+  def user
+    @user = User.find_by(email: params[:email])
+  end
+
+  def activate_and_log_in_user
+    user.activate
     log_in user
   end
 end
