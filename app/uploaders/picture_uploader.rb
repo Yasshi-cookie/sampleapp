0243e818009d0a56ff3e36e5b06e691a3907de4f
Yasshi-cookie <y.teruyacookie@gmail.com>
2020-06-04 15:23:48 +0900

    Add resize too big picture

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class PictureUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [400, 400]
+
   storage :file
 
   # アップロードファイルの保存先ディレクトリは上書き可能
